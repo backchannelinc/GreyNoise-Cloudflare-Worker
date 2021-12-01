d062f7cce32ef7f86b168acba1aac4fac62039d8
Aaron DeVera <aaron@backchannel.re>
2021-12-01 12:34:00 -0800

    git init

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # GreyNoise-Cloudflare-Worker
+
+## Running locally in development mode
+
+The best development experience for running locally is with [miniflare](https://miniflare.dev/), which does an excellent job simulating the Workers platform. 
+
+To do this, just run
+```
+miniflare index.js -w
+```
+This will run the default `wrangler.toml`, which is a template. To run a specific `wrangler.toml` that is perhaps filled with secrets like your GreyNoise API token and your Cloudflare ID info, you can specify a specific wrangler.toml by running something like this:
+```
+miniflare -c .secrets/wrangler.toml index.js -w
+```
+To simulate the environment variable `GREYNOISE_API_KEY` you will also need to pass it as a command line arg:
+```
+miniflare -c .secrets/wrangler.toml index.js -w --binding GREYNOISE_API_KEY=<your GreyNoise API key>
+```
+To route a tunnel to your localhost development runtime you could even use something like [cloudflared](https://github.com/cloudflare/cloudflared) or [ngrok](https://ngrok.com/) to better simulate web traffic.
---
+++ index.js
@@ -0,0 +1,98 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// getGreynoise
+// gets data from GreyNoise
+// inputs:
+//      ip: String
+// outputs:
+//      Object
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+async function getGreynoise(ip) {
+    
+    // here is how you would get you greynoise token if stored in a KV
+    //const GN_TOKEN = await your_worker_kv.get('GREYNOISE_API_KEY', {type:'text'})
+
+    // instead we will get an envrionment var
+    const GREYNOISE_TOKEN = GREYNOISE_API_KEY
+    const GN_COMMUNITY_URL = 'https://api.greynoise.io/v3/community/'
+    const GN_CONTEXT_URL = 'https://api.greynoise.io/v2/noise/context/'
+    const GN_RIOT_URL = 'https://api.greynoise.io/v2/riot/'
+    
+    var returnData
+  
+    const data = await fetch(`${GN_COMMUNITY_URL}${ip}`, {
+        headers: {
+            'Accept': 'application/json',
+            'key': GREYNOISE_TOKEN
+        }
+    })
+    const COMMUNITY_RESPONSE = data.json()
+    if (COMMUNITY_RESPONSE.noise === true) {
+      const contextData = await fetch(`${GN_CONTEXT_URL}${ip}`, {
+          headers: {
+              'Accept': 'application/json',
+              'key': GREYNOISE_TOKEN
+          }
+      })
+      const CONTEXT_RESPONSE = contextData.json()
+      returnData = CONTEXT_RESPONSE
+    } else {
+      returnData = COMMUNITY_RESPONSE
+    }
+    if (COMMUNITY_RESPONSE.riot === true) {
+      const riotData = await fetch(`${GN_RIOT_URL}${ip}`, {
+          headers: {
+              'Accept': 'application/json',
+              'key': GREYNOISE_TOKEN
+          }
+      })
+      const RIOT_RESPONSE = riotData.json()
+      returnData = RIOT_RESPONSE
+    } else {
+      returnData = COMMUNITY_RESPONSE
+    }
+    return returnData
+}
+
+async function maliciousResponse(){
+    const purgatory_url = 'https://rya.nc:1987'
+    return Response.redirect(purgatory_url,301)
+}
+
+async function benignResponse(){
+    return new Response("We're not so sure about you but here ya go.")
+}
+
+async function normalResponse(){
+    return new Response("Hello normal, nice human!")
+}
+
+
+async function handleRequest(request) {
+    
+    // Get the client IP for the incoming request
+    const clientIP = request.headers.get("CF-Connecting-IP")
+
+    // get the GreyNoise enrichment for this IP
+    const greynoiseResponse = await getGreynoise(clientIP)
+
+    // get GreyNoise classification
+    // this may be null or undefined, but thats ok, we fail open ;)
+    const greynoiseClassification = await greynoiseResponse['classification']
+
+    // let's go ahead and print classifications to the console
+    console.log(`[!] incoming request clientIP ${clientIP} has classification "${greynoiseClassification}" on GreyNoise: https://www.greynoise.io/viz/ip/${clientIP}`)
+
+    // routing conditions
+    if (greynoiseClassification === 'malicious'){
+        return maliciousResponse()
+    } else if (greynoiseClassification === 'benign'){
+        return benignResponse()
+    }
+    else {
+        return normalResponse()
+    }
+  }
+  
+  addEventListener("fetch", event => {
+    event.respondWith(handleRequest(event.request))
+  })
